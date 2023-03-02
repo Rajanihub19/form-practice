f8e733cf0aef586e7ed8861d54187ec1b36b41c6
Rajani <rajani@gmail.com>
2023-03-02 12:42:04 +0530

    form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FormDemo1 from './components/form/index'
+function App() {
+  return (
+    <>
+
+      <FormDemo1 />
+    </>
+  );
+}
+
+export default App;
